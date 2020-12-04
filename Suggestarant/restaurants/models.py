e205657256e50346f2f15c400b828e09fca5e772
alexanderJHarris <ajharris8814@gmail.com>
2020-12-04 15:04:05 -0500

    Add atrributes to Name model

--- Suggestarant/restaurants/models.py
+++ Suggestarant/restaurants/models.py
@@ -4,23 +4,8 @@ from django.db import models
 class Name(models.Model):
     restaurant_name = models.CharField(max_length=30)
     open_date = models.DateTimeField('date opened')
+    restaurant_address = models.CharField(max_length=100)
+    restaurant_rating = models.IntegerField(default = 0)
 
     def __str__(self):
         return self.restaurant_name
-
-
-class Address(models.Model):
-    restaurant_handle = models.ForeignKey(Name, on_delete=models.CASCADE)
-    restaurant_address = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.restaurant_address
-
-
-class Ratings(models.Model):
-    restaurant_handle = models.ForeignKey(Name, on_delete=models.CASCADE)
-    restaurant_ratings = models.IntegerField(
-        default=0)  # Change to accept floats later on!
-
-    def __str__(self):
-        return self.restaurant_ratings
