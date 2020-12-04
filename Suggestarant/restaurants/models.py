4596d74d1a59a531da6be97fb8fbccb6fbe048e4
alexanderJHarris <ajharris8814@gmail.com>
2020-12-03 23:27:59 -0500

    Add model for restaurants

--- Suggestarant/restaurants/models.py
+++ Suggestarant/restaurants/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Name(models.Model):
+    restaurant_name = models.CharField(max_length=30)
+    open_date = models.DateTimeField('date opened')
+
+    def __str__(self):
+        return self.restaurant_name
+
+
+class Address(models.Model):
+    restaurant_handle = models.ForeignKey(Name, on_delete=models.CASCADE)
+    restaurant_address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.restaurant_address
+
+
+class Ratings(models.Model):
+    restaurant_handle = models.ForeignKey(Name, on_delete=models.CASCADE)
+    restaurant_ratings = models.IntegerField(
+        default=0)  # Change to accept floats later on!
+
+    def __str__(self):
+        return self.restaurant_ratings
