425e3c721868294e9240cd66f3b11a169b65ed7e
alexanderJHarris <ajharris8814@gmail.com>
2020-12-10 23:55:05 -0500

    Abstract Location data from single field for later query

--- Suggestarant/restaurants/models.py
+++ Suggestarant/restaurants/models.py
@@ -3,9 +3,15 @@ from django.db import models
 
 class Name(models.Model):
     restaurant_name = models.CharField(max_length=30)
-    open_date = models.DateTimeField('date opened')
-    restaurant_address = models.CharField(max_length=100)
-    restaurant_rating = models.IntegerField(default = 0)
+    pub_date = models.DateTimeField('date published')
 
     def __str__(self):
         return self.restaurant_name
+
+class Location(models.Model):
+    restaurant = models.ForeignKey(Name, on_delete=models.CASCADE)
+    public_address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.public_address
+    
